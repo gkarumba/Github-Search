833c61048c0596dc6ab3b241382a0315deb37e04
gkarumba <gachegua@gmail.com>
2019-06-08 17:30:44 +0300

    Add the user profile & details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { UserProfileDetailsComponent } from './components/user-profile-details/user-profile-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    UserProfileDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/user-profile-details/user-profile-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-details',
+  templateUrl: './user-profile-details.component.html',
+  styleUrls: ['./user-profile-details.component.css']
+})
+export class UserProfileDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
