347e731944a250a094738b33857b9f581188b768
gkarumba <gachegua@gmail.com>
2019-06-08 17:59:23 +0300

    Add the User class elements

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,14 @@
 export class User {
+  showDetails: boolean;
+  constructor(
+    public username: string,
+    public followers: number,
+    public following: number,
+    public repos: number,
+    public stars: number,
+    public projects: number,
+    public profilePic: string
+  ) {
+    this.showDetails = false;
+  }
 }
